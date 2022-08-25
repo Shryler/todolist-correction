326cda7ac0a7c1c0fa576194616fd34e83d513d6
Shryler <105290336+Shryler@users.noreply.github.com>
2022-08-25 11:16:25 +0200

    correction

---
+++ src/components/Task/Task.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './task.css';
+import { FaTrashAlt } from 'react-icons/fa';
+
+const Task = (props) => {
+    return (
+        <div className="row no-gutters task-detail">
+            <div className="d-flex p-3 justify-content-between align-items-center">
+                <div className="form-check">
+                    <input type="checkbox" className="form-check-input" />
+                    <label className="form-check-label">
+                        {props.title}
+                    </label>
+                </div>
+                <button type='button' className="btn btn-sm btn-outline-danger d-flex">
+                    <FaTrashAlt />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Task;
