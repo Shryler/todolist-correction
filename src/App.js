af0667c3bf10c2bb3c81d02cd84c4169578bb8ad
Shryler <105290336+Shryler@users.noreply.github.com>
2022-08-25 13:43:24 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
+import { useState } from "react";
 import "./App.css";
 import Task from "./components/Task/Task";
+
 function App() {
+
+  const task1 = { title: "tache 1", completed: true };
+  const task2 = { title: "tache 2", completed: false };
+  const [tasksList, setTasksList] = useState([task1, task2]);
+  const tasksListElement = tasksList.map(task => {
+    return (<Task title={task.title} completed={task.completed} />);
+  })
+
   return (
     <div className="App container">
-      <Task title="Machin"></Task>
+      <div className="tasks-container">
+        {tasksListElement}
+      </div>
     </div>
   );
 }
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './task.css';
 import { FaTrashAlt } from 'react-icons/fa';
 
-const Task = (props) => {
+const Task = ({title, completed}) => {
     return (
         <div className="row no-gutters task-detail">
             <div className="d-flex p-3 justify-content-between align-items-center">
                 <div className="form-check">
-                    <input type="checkbox" className="form-check-input" />
+                    <input type="checkbox" className="form-check-input" checked={completed} />
                     <label className="form-check-label">
-                        {props.title}
+                        {title}
                     </label>
                 </div>
                 <button type='button' className="btn btn-sm btn-outline-danger d-flex">
